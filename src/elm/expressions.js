b165610e73f9474a64cae79f3886e2bf9f1e09f8
Chris Moesel <cmoesel@mitre.org>
2018-03-14 22:09:21 -0400

    Manual Coffee -> JS Step 2: Convert coffee to JS
    There are two files that decaffeinate cannot process.
    This is step two of the manual process to convert these files.

--- src/elm/expressions.js
+++ src/elm/expressions.js
@@ -1,30 +1,32 @@
-expression = require './expression',
-aggregate = require './aggregate',
-arithmetic = require './arithmetic',
-clinical = require './clinical',
-comparison = require './comparison',
-conditional = require './conditional',
-datetime = require './datetime',
-declaration = require './declaration',
-external = require './external',
-instance = require './instance',
-interval = require './interval',
-list = require './list',
-literal = require './literal',
-logical = require './logical',
-nullological = require './nullological',
-parameters = require './parameters',
-quantity = require './quantity',
-query = require './query',
-reusable = require './reusable',
-string = require './string',
-structured = require './structured',
-type = require './type',
-overloaded = require './overloaded'
+const expression = require('./expression');
+const aggregate = require('./aggregate');
+const arithmetic = require('./arithmetic');
+const clinical = require('./clinical');
+const comparison = require('./comparison');
+const conditional = require('./conditional');
+const datetime = require('./datetime');
+const declaration = require('./declaration');
+const external = require('./external');
+const instance = require('./instance');
+const interval = require('./interval');
+const list = require('./list');
+const literal = require('./literal');
+const logical = require('./logical');
+const nullological = require('./nullological');
+const parameters = require('./parameters');
+const quantity = require('./quantity');
+const query = require('./query');
+const reusable = require('./reusable');
+const string = require('./string');
+const structured = require('./structured');
+const type = require('./type');
+const overloaded = require('./overloaded');
 
-libs = [expression, aggregate, arithmetic, clinical, comparison, conditional, datetime, declaration,
-        external, instance, interval, list, literal, logical, nullological, parameters, query,quantity, reusable,
-        string, structured, type, overloaded]
-for lib in libs
-  for element in Object.keys(lib)
-    module.exports[element] = lib[element]
+const libs = [expression, aggregate, arithmetic, clinical, comparison, conditional, datetime, declaration,
+  external, instance, interval, list, literal, logical, nullological, parameters, query,quantity, reusable,
+  string, structured, type, overloaded];
+for (const lib of libs ) {
+  for (const element of Object.keys(lib)) {
+    module.exports[element] = lib[element];
+  }
+}
