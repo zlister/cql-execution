9bd97e1a857fd8ce3e39ef4ce0d3e9da893a679d
Chris Moesel <cmoesel@mitre.org>
2018-03-14 22:12:49 -0400

    Add bulk-decaffeinate config file

---
+++ bulk-decaffeinate.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mochaEnvFilePattern: '^.*-test.js$',
+  decaffeinateArgs: [
+    '--loose-default-params',
+    '--loose-for-expressions',
+    '--loose-for-of',
+    '--loose-includes'
+  ]
+};
